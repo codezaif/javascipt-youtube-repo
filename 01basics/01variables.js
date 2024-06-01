cacf0abd2f22f0138d6b8b1c78d69eb78b7bf492
HUZAIFA <144864554+codezaif@users.noreply.github.com>
2024-06-01 13:48:03 +0000

    variables understanding

---
+++ 01basics/01variables.js
@@ -0,0 +1,7 @@
+const AccountId = 12345
+let AccountName ="Huzaifa"
+let AccountEmail ="huzaifa@gm.com"
+let AccountPass ="helloworld"
+let AccountCity ="Multan"
+
+console.table([AccountId,AccountName,AccountEmail,AccountPass,AccountCity])
